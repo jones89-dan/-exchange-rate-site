97f8cd24bd00bff7f41bc2cc70125cbf4bc7df41
jones89-dan <jones.89.dan@gmail.com>
2021-12-22 11:47:51 -0800

    Changed header footer layout design, added css style to app.css

--- src/Layout.js
+++ src/Layout.js
@@ -7,19 +7,16 @@ import { MarkGithubIcon } from "@primer/octicons-react";
 const Layout = (props) => {
   return (
     <React.Fragment>
-      <nav className="navbar navbar-light bg-light">
-        <Link to="/"><span className="navbar-brand mb-0 h1">Currency Exchange Rates</span></Link>
-        <Link to="/singleConversion"><span className="navbar-brand mb-0 h1">Single Conversion</span></Link>
+      <nav className="navbar navbar-light bg-dark">
+        <Link to="/"><span className="navbar-brand mb-0 h1 text-white">Currency Exchange Rates</span></Link>
+        <Link to="/singleConversion"><span className="navbar-brand mb-0 h1 text-white">Single Conversion</span></Link>
       </nav>
       <div className="container py-3">
         {props.children}
       </div>
-      <footer className="p-3 bg-light">
-        <div className="mb-2">
-          <a className="badge badge-dark" href="https://github.com/Altcademy/exchange-rate-site" target="_blank" rel="noopener noreferrer">GitHub</a>
-        </div>
+      <footer className="p-3 bg-dark">
         <div>
-          <span className="mr-3 text-secondary">Built by <MarkGithubIcon size={16} /> <a href="https://github.com/jones89-dan" target="_blank" rel="noopener noreferrer">jones89-dan</a> </span>
+          <span className="mr-3 text-secondary text-white">Built by  jones89-dan <a href="https://github.com/jones89-dan" target="_blank" rel="noopener noreferrer"><MarkGithubIcon size={16} /></a> </span>
         </div>
       </footer>
     </React.Fragment>
