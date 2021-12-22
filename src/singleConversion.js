e67bad3324d6304b855a43817f7758d8dd95afcf
jones89-dan <jones.89.dan@gmail.com>
2021-12-22 11:04:05 -0800

    Added funcitons and compenants for updating currency base/amount and the converted base/amount

--- src/singleConversion.js
+++ src/singleConversion.js
@@ -23,6 +23,12 @@ class SingleConversion extends React.Component {
     this.chartRef = React.createRef();
   }
 
+  componentDidMount() {
+    const { baseAcronym, quoteAcronym } = this.state;
+    this.getRate(baseAcronym, quoteAcronym);
+    this.getHistoricalRates(baseAcronym, quoteAcronym);
+  }
+
   getRate = (base, quote) => {
     this.setState({ loading: true });
     fetch(`https://altexchangerateapi.herokuapp.com/latest?from=${base}&to=${quote}`)
@@ -44,6 +50,73 @@ class SingleConversion extends React.Component {
       })
       .catch(error => console.error(error.message));
   }
+
+  getHistoricalRates = (base, quote) => {
+    const endDate = new Date().toISOString().split('T')[0];
+    const startDate = new Date((new Date).getTime() - (30 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
+
+    fetch(`https://altexchangerateapi.herokuapp.com/${startDate}..${endDate}?from=${base}&to=${quote}`)
+      .then(checkStatus)
+      .then(json)
+      .then(data => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+
+        const chartLabels = Object.keys(data.rates);
+        const chartData = Object.values(data.rates).map(rate => rate[quote]);
+        const chartLabel = `${base}/${quote}`;
+        this.buildChart(chartLabels, chartData, chartLabel);
+      })
+      .catch(error => console.error(error.message));
+  }
+
+  toBase(amount, rate) {
+    return amount * (1 / rate);
+  }
+
+  toQuote(amount, rate) {
+    return amount * rate;
+  }
+
+  convert(amount, rate, equation) {
+    const input = parseFloat(amount);
+    if (Number.isNaN(input)) {
+      return '';
+    }
+    return equation(input, rate).toFixed(3);
+  }
+
+  changeBaseAcronym = (event) => {
+    const baseAcronym = event.target.value;
+    this.setState({ baseAcronym });
+    this.getRate(baseAcronym, this.state.quoteAcronym);
+    this.getHistoricalRates(baseAcronym, this.state.quoteAcronym);
+  }
+
+  changeBaseValue = (event) => {
+    const quoteValue = this.convert(event.target.value, this.state.rate, this.toQuote);
+    this.setState({
+      baseValue: event.target.value,
+      quoteValue,
+    });
+  }
+
+  changeQuoteAcronym = (event) => {
+    const quoteAcronym = event.target.value;
+    this.setState({ quoteAcronym });
+    this.getRate(this.state.baseAcronym, quoteAcronym);
+    this.getHistoricalRates(this.state.baseAcronym, quoteAcronym);
+  }
+
+  changeQuoteValue = (event) => {
+    const baseValue = this.convert(event.target.value, this.state.rate, this.toBase);
+    this.setState({
+      quoteValue: event.target.value,
+      baseValue,
+    });
+  }
+
 
 render() {
 
